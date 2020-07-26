1a670e5881282a7c569b71c52e48bbc082d793ae
Deon Hulett <deon.hulett@gmail.com>
2020-07-26 15:09:15 -0300

    Implements morse encoding algorithm

--- MorseCodeLib/morseencoder.cpp
+++ MorseCodeLib/morseencoder.cpp
@@ -1,5 +1,45 @@
 #include "morseencoder.h"
 
-QString morseEncode(const QString& text) {
-    return text;
+#include <QMap>
+#include <QTextStream>
+
+const QMap<QString, QString> morseCharacters =
+    {
+        {"a", ".-"},
+        {"b", "-..."},
+        {"c", "-.-."},
+        {"d", "-.."},
+        {"e", "."},
+        {"f", "..-."},
+        {"g", "--."},
+        {"h", "...."},
+        {"i", ".."},
+        {"j", ".---"},
+        {"k", "-.-"},
+        {"l", ".-.."},
+        {"m", "--"},
+        {"n", "-."},
+        {"o", "---"},
+        {"p", ".--."},
+        {"q", "--.-"},
+        {"r", ".-."},
+        {"s", "..."},
+        {"t", "-"},
+        {"u", "..-"},
+        {"v", "...-"},
+        {"w", ".--"},
+        {"x", "-..-"},
+        {"y", "-.--"},
+        {"z", "--.."},
+    };
+
+QString morseEncode(const QString &text)
+{
+    QString morseEncoded;
+    QTextStream stream(&morseEncoded);
+
+    for (const auto& character : text) {
+        stream << morseCharacters[character] << " ";
+    }
+    return morseEncoded.trimmed();
 }
